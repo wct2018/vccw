9f5a4d1cebd08420f0a296f8d6762be8f53c11bf
fumikito <guy@hametuha.com>
2017-02-27 13:22:59 +0900

    Force Jetpack debug true.

--- provision/playbooks/templates/extra-wp-config.php
+++ provision/playbooks/templates/extra-wp-config.php
@@ -1,4 +1,4 @@
-define( 'JETPACK_DEV_DEBUG', {{ vccw.wp_debug }} );
+define( 'JETPACK_DEV_DEBUG', TRUE );
 define( 'WP_DEBUG', {{ vccw.wp_debug }} );
 define( 'FORCE_SSL_ADMIN', {{ vccw.force_ssl_admin }} );
 define( 'SAVEQUERIES', {{ vccw.savequeries }} );
