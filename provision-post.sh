1d1f20a6201ed6ab117711046c4b6a07ca1f3726
fumikito <guy@hametuha.com>
2018-05-14 16:08:22 +0900

    ドメイン名などを変更

--- provision-post.sh
+++ provision-post.sh
@@ -9,7 +9,6 @@ if [ ! -e /var/www/html/wp-content/plugins/mu-plugins ]; then
 	svn co https://meta.svn.wordpress.org/sites/trunk/wordcamp.org/public_html/wp-content/mu-plugins/ /var/www/html/wp-content/mu-plugins
 fi
 
-
 # SVN plugins(May be not hosted on WP.org)
 SVN_PLUGINS=( camptix-network-tools email-post-changes tagregator )
 
@@ -27,14 +26,27 @@ if [ ! -e /var/www/html/wp-content/plugins/camptix ]; then
 fi
 wp plugin activate camptix
 
-# Install redux
-if [ ! -e /var/www/html/wp-content/themes/wordcamp-base-v2 ]; then
-	svn co https://meta.svn.wordpress.org/sites/trunk/wordcamp.org/public_html/wp-content/themes/wordcamp-base-v2/ /var/www/html/wp-content/themes/wordcamp-base-v2
+
+# WordPress.org meta plugins(Only in meta environment)
+META_PLUGINS=( camptix-attendance camptix-badge-generator camptix-mailchimp email-post-changes-specific-post jquery-ui-css multi-event-sponsors wc-canonical-years wc-fonts wc-post-types wcpt wordcamp-api wordcamp-coming-soon-page wordcamp-dashboard-widgets wordcamp-docs wordcamp-forms-to-drafts wordcamp-mentors wordcamp-organizer-reminders wordcamp-payments wordcamp-payments-network wordcamp-remote-css wordcamp-reports )
+for i in "${META_PLUGINS[@]}"
+do :
+	if [ ! -e /var/www/html/wp-content/plugins/$i ]; then
+		svn co https://meta.svn.wordpress.org/sites/trunk/wordcamp.org/public_html/wp-content/plugins/$i/ /var/www/html/wp-content/plugins/$i
+	fi;
+	wp plugin activate $i
+done
+
+
+# Install campsite-2017
+THEME_NAME=campsite-2017
+if [ ! -e /var/www/html/wp-content/themes/$THEME_NAME ]; then
+	svn co https://meta.svn.wordpress.org/sites/trunk/wordcamp.org/public_html/wp-content/themes/$THEME_NAME/ /var/www/html/wp-content/themes/$THEME_NAME
 fi
 
 # Original theme
-if [ ! -e /var/www/html/wp-content/themes/wct2017 ]; then
-	git clone https://github.com/wctokyo2017/wct2017.git /var/www/html/wp-content/themes/wct2017
-fi
-wp theme activate wct2017
+# if [ ! -e /var/www/html/wp-content/themes/wct2018 ]; then
+#	git clone https://github.com/wctokyo2018/wct2018.git /var/www/html/wp-content/themes/wct2018
+#fi
+#wp theme activate wct2018
 
